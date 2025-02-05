a95ba840a57955026a7632dbe90dd1e2c4cdda89
seungwoo7050 <seungwoo7050@naver.com>
2025-02-05 20:43:01 +0900

    feat(parse): 부호 있는 정수 입력 검증

--- src/parser.c
+++ src/parser.c
@@ -41,6 +41,73 @@ static int	count_all_tokens(int argc, char **argv)
 	return (count);
 }
 
+static int	parse_token(const char *arg, int start, int end, int *out)
+{
+	long long	value;
+	long long	limit;
+	int			sign;
+	int			i;
+
+	value = 0;
+	sign = 1;
+	i = start;
+	if (arg[i] == '+' || arg[i] == '-')
+	{
+		if (arg[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (i == end)
+		return (0);
+	limit = INT_MAX;
+	if (sign < 0)
+		limit = (long long)INT_MAX + 1;
+	while (i < end)
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+			return (0);
+		value = value * 10 + (arg[i] - '0');
+		if (value > limit)
+			return (0);
+		i++;
+	}
+	*out = (int)(value * sign);
+	return (1);
+}
+
+static int	fill_values(int argc, char **argv, t_stack *a)
+{
+	int	i;
+	int	pos;
+	int	start;
+	int	end;
+	int	value;
+
+	i = 1;
+	pos = 0;
+	while (i < argc)
+	{
+		end = 0;
+		while (argv[i][end] != '\0')
+		{
+			while (is_space(argv[i][end]))
+				end++;
+			if (argv[i][end] == '\0')
+				break ;
+			start = end;
+			while (argv[i][end] != '\0' && !is_space(argv[i][end]))
+				end++;
+			if (!parse_token(argv[i], start, end, &value))
+				return (0);
+			a->values[pos] = value;
+			a->ranks[pos] = value;
+			pos++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	parse_input(int argc, char **argv, t_stack *a)
 {
 	int	count;
@@ -53,6 +120,11 @@ int	parse_input(int argc, char **argv, t_stack *a)
 		return (0);
 	if (!stack_init(a, count))
 		return (0);
+	if (!fill_values(argc, argv, a))
+	{
+		stack_free(a);
+		return (0);
+	}
 	a->size = count;
 	return (1);
 }
