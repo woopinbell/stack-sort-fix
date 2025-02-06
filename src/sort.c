198ced11b40131161aba9b56935e0858bf639ded
seungwoo7050 <seungwoo7050@naver.com>
2025-02-06 21:12:09 +0900

    feat(sort): 작은 스택 전용 정렬 구현

--- src/sort.c
+++ src/sort.c
@@ -29,9 +29,68 @@ static void	move_index_to_top(t_stack *a, int index)
 	}
 }
 
+static void	sort_two(t_stack *a)
+{
+	if (a->ranks[0] > a->ranks[1])
+		op_sa(a, 1);
+}
+
+static void	sort_three(t_stack *a)
+{
+	int	first;
+	int	second;
+	int	third;
+
+	if (stack_is_sorted(a))
+		return ;
+	first = a->ranks[0];
+	second = a->ranks[1];
+	third = a->ranks[2];
+	if (first > second && second < third && first < third)
+		op_sa(a, 1);
+	else if (first > second && second > third)
+	{
+		op_sa(a, 1);
+		op_rra(a, 1);
+	}
+	else if (first > second && second < third && first > third)
+		op_ra(a, 1);
+	else if (first < second && second > third && first < third)
+	{
+		op_sa(a, 1);
+		op_ra(a, 1);
+	}
+	else if (first < second && second > third && first > third)
+		op_rra(a, 1);
+}
+
+static void	sort_tiny(t_stack *a, t_stack *b)
+{
+	int	target_rank;
+	int	index;
+
+	if (a->size == 2)
+	{
+		sort_two(a);
+		return ;
+	}
+	target_rank = 0;
+	while (a->size > 3)
+	{
+		index = find_rank_index(a, target_rank);
+		move_index_to_top(a, index);
+		op_pb(a, b, 1);
+		target_rank++;
+	}
+	sort_three(a);
+	while (b->size > 0)
+		op_pa(a, b, 1);
+}
+
 void	sort_stack(t_stack *a, t_stack *b)
 {
-	(void)b;
 	if (a->size < 2 || stack_is_sorted(a))
 		return ;
+	if (a->size <= 5)
+		sort_tiny(a, b);
 }
