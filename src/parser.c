cefd0f09c4f1659021b77a79c61e214ea2fbe3c8
seungwoo7050 <seungwoo7050@naver.com>
2025-02-06 17:04:25 +0900

    feat(parse): 중복 입력을 거절하고 상대 순위 계산

--- src/parser.c
+++ src/parser.c
@@ -108,6 +108,70 @@ static int	fill_values(int argc, char **argv, t_stack *a)
 	return (1);
 }
 
+static int	compare_ints(const void *left, const void *right)
+{
+	int	a;
+	int	b;
+
+	a = *(const int *)left;
+	b = *(const int *)right;
+	return ((a > b) - (a < b));
+}
+
+static int	find_rank(const int *sorted, int size, int value)
+{
+	int	low;
+	int	high;
+	int	mid;
+
+	low = 0;
+	high = size;
+	while (low < high)
+	{
+		mid = low + (high - low) / 2;
+		if (sorted[mid] < value)
+			low = mid + 1;
+		else
+			high = mid;
+	}
+	return (low);
+}
+
+static int	assign_ranks(t_stack *a)
+{
+	int	*sorted;
+	int	i;
+
+	sorted = (int *)malloc(sizeof(int) * (size_t)a->size);
+	if (sorted == NULL)
+		return (0);
+	i = 0;
+	while (i < a->size)
+	{
+		sorted[i] = a->values[i];
+		i++;
+	}
+	qsort(sorted, (size_t)a->size, sizeof(int), compare_ints);
+	i = 1;
+	while (i < a->size)
+	{
+		if (sorted[i - 1] == sorted[i])
+		{
+			free(sorted);
+			return (0);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < a->size)
+	{
+		a->ranks[i] = find_rank(sorted, a->size, a->values[i]);
+		i++;
+	}
+	free(sorted);
+	return (1);
+}
+
 int	parse_input(int argc, char **argv, t_stack *a)
 {
 	int	count;
@@ -126,5 +190,10 @@ int	parse_input(int argc, char **argv, t_stack *a)
 		return (0);
 	}
 	a->size = count;
+	if (!assign_ranks(a))
+	{
+		stack_free(a);
+		return (0);
+	}
 	return (1);
 }
